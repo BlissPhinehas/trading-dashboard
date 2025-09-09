ada675226e6624dc9a3e8b88f8f4bb427692d41b
BlissPhinehas <evansbliss17@gmail.com>
2025-09-08 23:26:43 -0400

    feat: backend successfully running with H2 database and Spring Security

---
+++ backend/src/test/java/com/bliss/tradingdashboard/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bliss.tradingdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
